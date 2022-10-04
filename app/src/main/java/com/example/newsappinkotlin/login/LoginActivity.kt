ae2dbff58072a0520ec744d003ea36e51db79fe3
Pratik Dhage <dhagepratik94@gmail.com>
2022-10-04 13:56:26 +0530

    Register activity

---
+++ app/src/main/java/com/example/newsappinkotlin/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newsappinkotlin.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newsappinkotlin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
